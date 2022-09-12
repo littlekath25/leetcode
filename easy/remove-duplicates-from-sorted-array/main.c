95f004f05af135409cc036a323a0097eb6f5f1a4
Katherine <katherine-fu@outlook.com>
2022-09-12 11:56:52 +0200

    start

---
+++ easy/remove-duplicates-from-sorted-array/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize){
+
+}
+
+int main(void) {
+  int nums[10] = {1, 2, 1, 2, 3, 4, 5, 6, 3, 7, 8};
+  int total = 7;
+
+  removeDuplicates(nums, total);
+}
